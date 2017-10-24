939865baafc9b1fb052cfa2f87592c0dee5b0f6a
stoychevvasko <stoychev.vassil@gmail.com>
2017-10-24 17:53:32 +0300

    cone [...]
    Signed-off-by: stoychevvasko <stoychev.vassil@gmail.com>

--- Learning Python/2017.09.28-TILs/demo/shapes/cone.py
+++ Learning Python/2017.09.28-TILs/demo/shapes/cone.py
@@ -1,4 +1,4 @@
-"""Cone class."""
+"""Cone class (right, circular)."""
 
 # import math
 from circle import Circle
@@ -12,14 +12,28 @@ class Cone(Shape):
         """Initializes a new Cone instance."""
         super(Cone, self).__init__(c_base.x_coord, c_base.y_coord, c_base.z_coord)
         self._apex = Point(apex.x_coord, apex.y_coord, apex.z_coord)
-        self.base = Circle(c_base.x_coord, c_base.y_coord, c_base.z_coord, r_radius)
+        self._base = Circle(c_base.x_coord, c_base.y_coord, c_base.z_coord, r_radius)
 
-        @property
-        def base(self):
-            """Getter for self._base."""
-            return self._base
+    @property
+    def apex(self):
+        """Getter for self._apex."""
+        return self._apex
 
-        @base.setter
-        def base(self, value):
-            """Setter for self._base."""
-            self._base = value
+    @apex.setter
+    def apex(self, value):
+        """Setter for self._apex."""
+        self._apex = value
+
+    @property
+    def base(self):
+        """Getter for self._base."""
+        return self._base
+
+    @base.setter
+    def base(self, value):
+        """Setter for self._base."""
+        self._base = value
+
+    def get_volume(self):
+        """Returns cone volume."""
+        return (1.0/3.0)*self.base.get_surface()*Point.get_distance(self.base.center, self.apex)
--- Learning Python/2017.09.28-TILs/demo/shapes/test_cone.py
+++ Learning Python/2017.09.28-TILs/demo/shapes/test_cone.py
@@ -2,6 +2,7 @@
 
 from circle import Circle
 from cone import Cone
+from point import Point
 # pylint:disable=unused-import
 from test_fixtures import cone_sm, cone_bg
 
@@ -13,12 +14,27 @@ class TestCone(object):
     def test_cone__init__(self, cone_sm, cone_bg):
         """Tests constructor."""
         assert cone_sm is not None
-        assert isinstance(cone_sm, Cone), 'invalid type exception was raised!'
         assert cone_bg is not None
-        assert isinstance(cone_sm, Cone), 'invalid type exception was raised!'
+        assert isinstance(cone_sm, Cone)
+        assert isinstance(cone_sm, Cone)
 
     # pylint:disable=redefined-outer-name
-    def test_cone_base_property(self, cone_sm):
+    def test_cone_base_property(self, cone_sm, cone_bg):
         """Tests the base property."""
         assert cone_sm.base is not None
+        assert cone_bg.base is not None
         assert isinstance(cone_sm.base, Circle)
+        assert isinstance(cone_bg.base, Circle)
+
+    def test_cone_apex_property(self, cone_sm, cone_bg):
+        """Tests the apex (vertex) property."""
+        assert cone_sm.apex is not None
+        assert cone_bg.apex is not None
+        assert isinstance(cone_sm.apex, Point)
+        assert isinstance(cone_bg.apex, Point)
+
+    # pylint:disable=redefined-outer-name
+    def test_cone_get_volume(self, cone_sm, cone_bg):
+        """Tests the get_volume() method."""
+        assert cone_sm.get_volume() > 0
+        assert cone_bg.get_volume() > 0
